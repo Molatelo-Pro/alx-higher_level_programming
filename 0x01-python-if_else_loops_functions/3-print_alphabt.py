b26d68af6139ce3e778bfcde1cfa580c5b5c75e6
Molatelo-Pro <molatelomashishi@gmail.com>
2024-02-02 14:58:49 -0800

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(96, 124):
-if i != 100 and i != 112:
-print("{}".format(chr(i)), end="")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
