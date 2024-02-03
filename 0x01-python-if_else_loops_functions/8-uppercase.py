0da6d20e239fb29b58c12b03d9782bbe570c2130
Molatelo-Pro <molatelomashishi@gmail.com>
2024-02-02 16:04:30 -0800

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+ result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    return result
+       print("{:s}".format(char), end="")
+    print('')
