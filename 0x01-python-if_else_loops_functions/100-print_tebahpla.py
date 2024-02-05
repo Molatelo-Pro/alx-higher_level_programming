dc8336b8bb8e892d87c274dfcffd6de5c89b62a1
Molatelo-Pro <molatelomashishi@gmail.com>
2024-02-05 15:44:31 -0800

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    print("{}".format(chr(i)), end="")
+for i in range(ord('z'), ord('a') - 1, -2):
+    print("{:c}{:s}".format(i, chr(i - 33)), end="")
