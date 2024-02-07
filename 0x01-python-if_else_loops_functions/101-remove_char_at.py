926b7e7874fa4322596d63bba927265917949855
Molatelo-Pro <molatelomashishi@gmail.com>
2024-02-06 22:12:10 -0800

    Remove at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pytho3
+def remove_char_at(str, n):
+    original_str = ''
+    j = 0
+    for c in str:
+        if j != n:
+            original_str += str[i]
+        j += 1
+    return original_str
