38c6471878284cda850acb26624692edda4b785a
Molatelo-Pro <molatelomashishi@gmail.com>
2024-02-07 22:51:19 -0800

    Remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 def remove_char_at(str, n):
     if 0 <= n < len(str):
         return str[:n] + str[n+1:]
-    elif n < 0 and abs(n) <= len(str):
+    elif n < 0 and abs(n) <= -len(str):
         return str[:n] + str[n+1:]
     else:
         return str
