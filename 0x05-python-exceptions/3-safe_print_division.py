575e5d56a564f6f252350f626714a2551ff3a5dc
Molatelo-Pro <molatelomashishi@gmail.com>
2024-02-19 13:26:05 -0800

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Error: Division by zero")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        print("Inside result: {}".format(result))
+        return (result)
