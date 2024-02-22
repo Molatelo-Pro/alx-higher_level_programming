61da87fee6a49e5ed70956fb3f8b129c611b9ab3
Molatelo-Pro <molatelomashishi@gmail.com>
2024-02-22 07:29:52 -0800

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -21,5 +21,5 @@ class Square:
             self.__size = size
 
     def area(self):
-
-            return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size ** 2)
