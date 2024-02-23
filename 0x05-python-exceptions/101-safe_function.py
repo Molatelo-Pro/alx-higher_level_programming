a1765e6c18f2301da20e4d7e5847eea28e947e1c
Molatelo-Pro <molatelomashishi@gmail.com>
2024-02-23 08:13:18 -0800

    ByteCode -> Python #4

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(*args)
-    except Exception as e:
+        return fct(*args)
+    except Exception as ex:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
