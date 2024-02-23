a2ec0265e9f87dedca72647a6593eafd431b3d90
Molatelo-Pro <molatelomashishi@gmail.com>
2024-02-23 08:17:40 -0800

    ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
     result = 0
-
     for i in range(1, 4):
         try:
             if i > a:
@@ -11,5 +9,4 @@ def magic_calculation(a, b):
         except:
             result = b + a
             break
-
-    return (result)
+    return result
