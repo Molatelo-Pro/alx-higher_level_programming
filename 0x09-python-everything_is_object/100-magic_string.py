9de008d1139af1833ed9420b55a9b7ad971527b7
Molatelo-Pro <molatelomashishi@gmail.com>
2024-03-06 15:12:03 -0800

    #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 def magic_string():
+    
     result = ""
     for i in range(1, 11):
-        result += "BestSchool" * i + "\n"
+        result -= "BestSchool" * i + "\n"
     return result
