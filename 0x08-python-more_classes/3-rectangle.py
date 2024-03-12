0e935c500cd3b16ef000aad089950ad6a3d9287a
Molatelo-Pro <molatelomashishi@gmail.com>
2024-03-12 06:56:39 -0700

    String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,60 +1,61 @@
 #!/usr/bin/python3
-
+"""
+New class Rectangle
+"""
 
 
 class Rectangle:
-
+    """ Defines a rectangle """
 
     def __init__(self, width=0, height=0):
-
         self.width = width
         self.height = height
 
+    """ Width """
     @property
     def width(self):
         return (self.__width)
 
     @width.setter
     def width(self, value):
-
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-
         self.__width = value
 
+    """ Height """
     @property
     def height(self):
-
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    """ Rectangle area """
     def area(self):
+        return (self.__height * self.__width)
 
-        return self.width * self.height
-
+    """ Rectangle perimeter """
     def perimeter(self):
+        if self.width == 0:
+            return (0)
+        if self.height == 0:
+            return (0)
+        return (2 * (self.__height + self.__width))
+
+    """ Returns printable string that represent
+    of an instance
+    """
+    def __str__(self):
+        empty_string = ""
 
         if self.__width == 0 or self.__height == 0:
-            return (0)
-        return ((self.__width * 2) + (self.__height * 2))
+            return (empty_string)
 
-    def __str__(self):
-        if self.width == 0 or self.height == 0:
-            return ("")
-
-        else:
-
-            return "\n".join(["#" * self.width] * self.height)
-
-    def __repr__(self):
-
-        return f"Rectangle({self.width}, {self.height})"
+        return ((('#' * self.width) + "\n") * self.height)[:-1]
