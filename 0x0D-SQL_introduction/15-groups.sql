82348ea47d977238af990c42248fb8b73a450300
Molatelo-Pro <molatelomashishi@gmail.com>
2024-06-11 06:44:27 -0700

    Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,9 +1,6 @@
 --number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL
 --number of records in descending count
 -- list_scores.sql
-
-USE hbtn_0c_0;
-
 SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`
